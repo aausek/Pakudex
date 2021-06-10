1e7ce5683563bb66bdc6b8dd49f9c7adbb4f9844
Ana A <aausek@users.noreply.github.com>
2021-06-10 15:12:49 -0400

    Updating class and list function

--- pakudex.py
+++ pakudex.py
@@ -2,6 +2,7 @@
 from pakuri import Pakuri
 import os
 
+
 def main():
     print('Welcome to Pakudex: Let''s Go!\n')
     printMenu()
@@ -16,36 +17,54 @@ def printMenu():
 4. Remove Pakuri\n \
 5. Change Pakuri Level\n \
 6. Exit\n')
-    selection = input('What would you like to do?')
-    menuSelection(selection);
+    selection = int(input('What would you like to do? '))
+    menuSelection(selection)
 
 
 def menuSelection(selection):
-
     # If 1 list pakuri
-    if selection == '1':
-        Operation('list')
+    if selection == 1:
+        # Operation('list')
+        List()
+        printMenu()
     # If 2 show pakuri
-    elif selection == '2':
-        Operation('show')
-    # If 3 add pakuri
-    elif selection == '3':
-        Operation('add')
-    # If 4 remove pakuri
-    elif selection == '4':
-        Operation('remove')
-    # If 5 change pakuri
-    elif selection == '5':
-        Operation('change')
+    # elif selection == 2:
+    #     Operation('show')
+    # # If 3 add pakuri
+    elif selection == 3:
+        Add()
+        printMenu()
+    # # If 4 remove pakuri
+    # elif selection == 4:
+    #     Operation('remove')
+    # # If 5 change pakuri
+    # elif selection == 5:
+    #     Operation('change')
     # If 6 print exit message and quit program
-    elif selection == '6':
-        print('Thanks for using Pakudex: Let''s Go! Bye!')
-        exit()
+    elif selection == 6:
+        print('\nThanks for using Pakudex: Let''s Go! Bye!')
+        exit(0)
     else:
-        print('Unrecognized menu selection!\n')
+        print('\nUnrecognized menu selection!\n')
         printMenu()
 
-def Operation(oper):
+
+def List():
+    obj = Pakuri.instances
+    if obj:
+        for num, ob in enumerate(obj):
+            print(num + ' ' + ob.name + '(' + ob.species + ', level ' + ')')
+    else:
+        print('\nNo Pakuri in Pakudex yet!\n')
+
+
+def Add():
+    
+
+# def Show()
+# def Add()
+# def Remove()
+# def Change Level()
 
 
 # Set main()
--- pakuri.py
+++ pakuri.py
@@ -2,23 +2,54 @@
 
 # Class
 class Pakuri:
+    instances = set()
+    pkCount = 0
+
     # Constructor
     def __init__(self, name, species, level=0):
         self.__name = name
         self.__species = species
+        # self.__hp = hp
+        # self.__cp = cp
         self.level = level
+        Pakuri.pkCount += 1
+        Pakuri.instances.add(self)
 
-    # Level getter method
-    def get_level(self):
+    @property
+    def name(self):
+        return self.__name
+
+    @property
+    def species(self):
+        return self.__species
+
+    # @property
+    # def hp(self):
+    #     return self.__hp
+    
+    # @property
+    # def cp(self):
+    #     return self.__cp    
+
+    @property
+    def level(self):
         return self.level
 
-    # Level setter method
-    def set_temperature(self, level):
-        if level < 0:
-            raise ValueError('Level cannot be negative.')
-        elif level > 500:
-            raise ValueError('Maximum level for Pakuri is 50.')
-        elif level != int:
-            raise ValueError('Invalid level!')
-        else:
-            self.level = level
+    @level.setter
+    def level(self, level):
+        self.level = level
+
+    # # Level setter method
+    # def set_level(self, level):
+    #     if level < 0:
+    #         raise ValueError('Level cannot be negative.')
+    #     elif level > 500:
+    #         raise ValueError('Maximum level for Pakuri is 50.')
+    #     elif level != int:
+    #         raise ValueError('Invalid level!')
+    #     else:
+    #         self.level = level
+
+    def add(self, name, species, hp, cp, level=0):
+        obj = Pakuri(name, species, hp, cp)
+        instances.add(obj)
